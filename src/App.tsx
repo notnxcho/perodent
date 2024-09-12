f68b061a8b184417dd537dd006bc25fe448afa31
notnxcho <nacholorenzo.650@gmail.com>
2024-09-12 16:04:36 -0300

    shortcuts image

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 
 import './App.css';
 import PeriodontalChart from './components/PeriodontalChart/PeriodontalChart';
+import shortcutsImage from './assets/shortcuts.png'
+
 
 function App() {
   return (
     <div className="flex flex-col w-screen min-h-screen items-center justify-center gap-4 ">
       <PeriodontalChart />
+      <img src={shortcutsImage} height={188} width={609}/>
     </div>
   );
 }
