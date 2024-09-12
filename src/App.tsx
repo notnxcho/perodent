a539692e3d63453748d6a69ca2497fcd0c7766cd
notnxcho <nacholorenzo.650@gmail.com>
2024-09-12 16:07:15 -0300

    trying to fix images

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 
 import './App.css';
 import PeriodontalChart from './components/PeriodontalChart/PeriodontalChart';
-import shortcutsImage from './assets/shortcuts.png'
+import keystrokes from './assets/keystrokes.png'
 
 
 function App() {
   return (
     <div className="flex flex-col w-screen min-h-screen items-center justify-center gap-4 ">
       <PeriodontalChart />
-      <img src={shortcutsImage} height={188} width={609}/>
+      <img src={keystrokes} height={188} width={609}/>
     </div>
   );
 }
